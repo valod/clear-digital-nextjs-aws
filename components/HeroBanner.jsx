bdeb6a2e45be9be06aa331c4d2a3a9ddfaa67060
manish chauhan <manish.c@cleardigital.com>
2023-12-10 01:35:43 +0530

    fix homepage

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -18,9 +18,9 @@ const HomepageHero = () => {
           </Link>
         </div>
       </div>
-      <div className={`${styles['bg-img']} bg-container overlay`} data-astro-cid-g47crtsm="" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
+      <div className={`${styles['bg-img']} bg-container overlay`} style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
         <video className="video-bg overlay" poster="https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp" autoPlay loop muted data-astro-cid-g47crtsm="" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
-          <source src="https://media.istockphoto.com/id/1372283447/video/attractive-woman-wearing-virtual-reality-headset-augmented-reality-creative-lady-swiping.mp4?s=mp4-640x640-is&amp;k=20&amp;c=Flz0Jza76gPaFZyEJ8X6114fy__lJc1m_4_yKZRZsVo=" type="video/mp4" data-astro-cid-g47crtsm="" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}></source>
+          <source src="https://media.istockphoto.com/id/1372283447/video/attractive-woman-wearing-virtual-reality-headset-augmented-reality-creative-lady-swiping.mp4?s=mp4-640x640-is&amp;k=20&amp;c=Flz0Jza76gPaFZyEJ8X6114fy__lJc1m_4_yKZRZsVo=" type="video/mp4" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}></source>
         </video>
       </div>
     </section>
