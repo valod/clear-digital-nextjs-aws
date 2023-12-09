15b1e7966710fc96a4bdc45f7b7837cfd2b68dfe
manish chauhan <manish.c@cleardigital.com>
2023-12-10 02:08:19 +0530

    fix homepage

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -4,7 +4,7 @@ import styles from './styles/herobanner.module.css';
 
 const HomepageHero = () => {
   return (
-    <section id="HomepageHero" className={`${styles.herobanner} min-h-screen`}>
+    <section id="HomepageHero" className={`${styles.herobanner} min-h-screen `}>
       <div className="container">
         <div className={`${styles.content_wrap}`}>
           <div className="subtitle">
@@ -19,7 +19,7 @@ const HomepageHero = () => {
         </div>
       </div>
       <div className={`${styles['bg-img']} bg-container overlay`} style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
-        <video className="video-bg overlay" poster="https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp" autoPlay loop muted data-astro-cid-g47crtsm="" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
+        <video className="video-bg overlay" poster="https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp" autoPlay loop muted style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}>
           <source src="https://media.istockphoto.com/id/1372283447/video/attractive-woman-wearing-virtual-reality-headset-augmented-reality-creative-lady-swiping.mp4?s=mp4-640x640-is&amp;k=20&amp;c=Flz0Jza76gPaFZyEJ8X6114fy__lJc1m_4_yKZRZsVo=" type="video/mp4" style={{ '--cssImg': 'url(https://dev-clear-astro-wp.pantheonsite.io/wp-content/uploads/2023/10/iStock-1280582431-1.webp)' }}></source>
         </video>
       </div>
