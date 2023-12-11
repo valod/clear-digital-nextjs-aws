19c72ffecbe8e004dc5b2a52f4c3f5b1c318034f
manish chauhan <manish.c@cleardigital.com>
2023-12-11 23:01:31 +0530

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+// import Image from 'next/image';
+// import styles from './page.module.css';
 import { NextSeo } from 'next-seo';
 import Layout from '../components/layout';
 import ColTwoSlider from '../components/colTwoSlider';
