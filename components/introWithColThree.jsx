cfd3bd792872592d7ac9874167a42c3f1da311d6
Ganesh <ganesh@webenertia.com>
2023-12-11 19:51:49 +0530

    update intro with col three

--- components/introWithColThree.jsx
+++ components/introWithColThree.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Styles from './styles/introWithColThree.module.css'
 import Image from 'next/image'
+
 function IntroWithColThree() {
-    return (
-        <section className={`${Styles.introWithColThree} black_square_pattern`}>
+
+    return (<>
+        <style jsx>{`
+
+.slick-list{
+    width: 100% !important;
+  }
+        /* Add more media queries as needed */
+      `}</style>
+
+        <section className={`${Styles.introWithColThree} black_square_pattern min-h-[1090px]`}>
             <div className="container">
-                <div className="w-full text-black mb-12">
+                <div className="w-full text-black mb-12 max-w-[1161px]">
                     <span className="subtitle mb-5"> 20+ years of success.</span>
                     <h2 className='mb-12'>We deliver results-driven digital experiences that get you to what&#39;s <span>next</span>.</h2>
                     <p>Clear Digital is a Silicon Valley-based website design agency that helps you build and accelerate your web, brand, and digital marketing by providing expert, strategic, and data-informed solutions that deliver results.</p>
@@ -41,6 +51,7 @@ function IntroWithColThree() {
                 </div>
             </div>
         </section>
+    </>
     )
 }
 
