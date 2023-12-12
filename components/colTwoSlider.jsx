a279af8e16ed6aa40076dd1bae375054dae65b63
Ganesh <ganesh@webenertia.com>
2023-12-12 12:26:35 +0530

    fixing build

--- components/colTwoSlider.jsx
+++ components/colTwoSlider.jsx
@@ -7,14 +7,14 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function ColTwoSlider() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        arrows: true
-    };
+    // const settings = {
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 500,
+    //     slidesToShow: 2,
+    //     slidesToScroll: 1,
+    //     arrows: true
+    // };
 
     return (
         <section className={`${Styles.ColTwoSlider} relative bg-black p-0`}>
@@ -26,7 +26,7 @@ function ColTwoSlider() {
                     </div>
                     <div className="w-full h-full relative flex flex-col justify-end mx-2.5">
                         <div className={`${Styles.logoWrap} w-full h-full mb-10`}>
-                            <Image className={``} width={176} height={16} src='/logo-digimarc.webp' alt="digimarc_ogo" />
+                            <Image width={176} height={16} src='/logo-digimarc.webp' alt="digimarc_ogo" />
                         </div>
                         <h2 className='text-white max-w-[370px]'><span className='circle-around'>27%</span> increase on click-through rates.</h2>
                         <div className="btn-wrap mt-10">
@@ -40,7 +40,7 @@ function ColTwoSlider() {
                     </div>
                     <div className="w-full h-full relative flex flex-col justify-end mx-2.5">
                         <div className={`${Styles.logoWrap} w-full h-full mb-10`}>
-                            <Image className={``} width={176} height={16} src='/logo-leadspace.webp' alt="digimarc_ogo" />
+                            <Image width={176} height={16} src='/logo-leadspace.webp' alt="digimarc_ogo" />
                         </div>
                         <h2 className='text-white max-w-[470px]'>53% <span className='zigzag more-wow'>increase</span> on engagement metrics.</h2>
                         <div className="btn-wrap mt-10">
