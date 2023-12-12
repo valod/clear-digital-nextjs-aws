bf1385bbcf48fae5c299ff73c329fbc862b6e700
Ganesh <ganesh@webenertia.com>
2023-12-12 13:29:38 +0530

    update google font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
 import './globals.css';
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return <>
+		<Head>
+			<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
+			<link href='https://fonts.googleapis.com/css?family=Permanent Marker' rel='stylesheet' />
+		</Head>
+		<Component {...pageProps} />
+	</>;
 }
 
 export default MyApp;
