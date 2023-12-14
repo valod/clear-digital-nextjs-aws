52952bbfbbfa6af681a6ec13be7c0e854960a768
Ganesh <ganesh@webenertia.com>
2023-12-14 18:03:25 +0530

    matched scrollable content

--- components/scrollableContent.jsx
+++ components/scrollableContent.jsx
@@ -6,16 +6,16 @@ export default function ScrollableContent() {
     return (
         <section class="content-with-img no-padding">
             <div class="column-wrapper">
-                <div class="inner-wrap flex items-center bg-gray">
+                <div class="inner-wrap flex bg-gray">
                     <div class="text-holder w-1/2 pl-[95px] pr-[135px]">
-                        <div class="inner-content">
-                            <div class="title-wrap">
-                                <p class="subtitle">Our process</p>
+                        <div class="inner-content h-full flex flex-col justify-between">
+                            <div class="title-wrap pt-[149px]">
+                                <h6 className="mb-[19px]">Our process</h6>
                                 <h3>Nisl <span className='text-pink'>pulvinar</span> consectet nec set adipiscing</h3>
                             </div>
-                            <div class="body-wrap mt-32">
+                            <div class="body-wrap pb-[151px]">
                                 <h4>Lorem ipsum dolor sit amet consectetur</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
+                                <p className="mt-[19px]">Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
                             </div>
                         </div>
                     </div>
@@ -26,16 +26,16 @@ export default function ScrollableContent() {
                     </div>
 
                 </div>
-                <div class="inner-wrap flex items-center bg-gray">
+                <div class="inner-wrap flex bg-gray">
                     <div class="text-holder w-1/2 pl-[95px] pr-[135px]">
-                        <div class="inner-content">
-                            <div class="title-wrap">
-                                <p class="subtitle">Our process</p>
+                        <div class="inner-content h-full flex flex-col justify-between">
+                            <div class="title-wrap pt-[149px]">
+                                <h6 className="mb-[19px]">Our process</h6>
                                 <h3>Nisl <span className='text-pink'>pulvinar</span> consectet nec set adipiscing</h3>
                             </div>
-                            <div class="body-wrap mt-32">
+                            <div class="body-wrap pb-[151px]">
                                 <h4>Lorem ipsum dolor sit amet consectetur</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
+                                <p className="mt-[19px]">Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
                             </div>
                         </div>
                     </div>
@@ -46,16 +46,16 @@ export default function ScrollableContent() {
                     </div>
 
                 </div>
-                <div class="inner-wrap flex items-center bg-gray">
+                <div class="inner-wrap flex bg-gray">
                     <div class="text-holder w-1/2 pl-[95px] pr-[135px]">
-                        <div class="inner-content">
-                            <div class="title-wrap">
-                                <p class="subtitle">Our process</p>
+                        <div class="inner-content h-full flex flex-col justify-between">
+                            <div class="title-wrap pt-[149px]">
+                                <h6 className="mb-[19px]">Our process</h6>
                                 <h3>Nisl <span className='text-pink'>pulvinar</span> consectet nec set adipiscing</h3>
                             </div>
-                            <div class="body-wrap mt-32">
+                            <div class="body-wrap pb-[151px]">
                                 <h4>Lorem ipsum dolor sit amet consectetur</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
+                                <p className="mt-[19px]">Lorem ipsum dolor sit amet consectetur. Pharetra dictum faucibus proin turpis sit. Tortor amet sit nunc libero non nec suscipit volutpat in. Viverra adipiscing dolor amet vitae. Malesuada vitae consectetur erat risus ac nibh. Lectus egestas adipiscing interdum hendrerit vitae consequat odio. Mattis imperdiet vitae tellus fringilla. Magna.</p>
                             </div>
                         </div>
                     </div>
