4f806616cc7c79fa3366fc6afb56ce27e80156c5
Abhay <abhay@cleardigital.com>
2023-12-14 17:18:32 +0530

    update footercta

--- components/Footercta.jsx
+++ components/Footercta.jsx
@@ -10,8 +10,8 @@ const FooterCta = () => {
       </div>
       <div className='container'>
         <div className="">
-          <h6 className='text-white'>Lorem ipsum dolor.</h6>
-          <h2 className='h1'>Let&#39;s talk.</h2>
+          <h6 className='text-white font-normal mb-[20px]'>Lorem ipsum dolor.</h6>
+          <h2 className='h1 mb-[30px]'>Let&#39;s talk.</h2>
           <div>
             <Link className="pink-btn" href="#">Contact us</Link>
           </div>
