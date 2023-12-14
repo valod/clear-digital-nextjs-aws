4a70284bccace7c4ca4999930f45795ec4853fb9
Ganesh <ganesh@webenertia.com>
2023-12-14 15:50:16 +0530

    update

--- components/logowithcontentServiceDetail.jsx
+++ components/logowithcontentServiceDetail.jsx
@@ -8,7 +8,7 @@ const data = [
     logo: '/cisco.svg',
     heading: '80% vel consequat est. Donec semper neque quam eget.',
     para: 'Torem ipsum dolor sit amet, consectetur adipiscing at elit. Nunc vulputate libero dolore aliqua et.',
-    border: true
+    border: false
   },
   {
     bgimg: '/cardsbg.svg',
@@ -33,23 +33,23 @@ const data = [
 
 const Logowithcontent = () => {
   return (
-    <section className="logowithcontent pb-0">
-  
+    <section className="logowithcontent py-0">
+
 
       {data.map((wrap, index) => (
         <div className={`${wrap.border == true ? `border-t border-gray` : ''}area w-full relative`} key={index}>
           {/* Use wrap.bgimg instead of bgimg */}
           {wrap.bgimg && (
             <div className={`bgimg ${style.bgimg}`}>
-              <Image src={wrap.bgimg} className="w-full h-full object-cover" width={100} height={100} alt='image'/>
+              <Image src={wrap.bgimg} className="w-full h-full object-cover" width={100} height={100} alt='image' />
             </div>
           )}
 
           <div className="container">
             <div className={`wrapper ${style.wrapper} w-full relative flex items-center flex-wrap h-[363px]  md:h-[310px]`}>
-              <div className="imgwrapper w-[50%] pl-[120px] laptop:pl-[60px] largedesktop:pl-[120px] tablet:pl-[50px] phablet:pl-[30px] sm:w-[100%] pl-[20px] ">
+              <div className="imgwrapper w-[50%] pl-[120px] laptop:pl-[60px] largedesktop:pl-[120px] tablet:pl-[50px] phablet:pl-[30px] sm:w-[100%] sm:pl-[20px] ">
                 <div className="logowrapper max-w-[190px] max-h-[69px]">
-                  <Image className="w-full h-full object-cover" width={100} height={100} src={wrap.logo} alt='image'/>
+                  <Image className="w-full h-full object-cover" width={100} height={100} src={wrap.logo} alt='image' />
                 </div>
               </div>
               <div className="contentwrapper w-[50%] sm:w-[100%]">
