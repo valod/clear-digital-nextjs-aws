fd31e770c6242e3b356f7c0780df66dfe423cbb5
Abhay <abhay@cleardigital.com>
2023-12-15 13:26:15 +0530

    update typography

--- components/Footercta.jsx
+++ components/Footercta.jsx
@@ -10,8 +10,8 @@ const FooterCta = ({ props }) => {
       </div>
       <div className='container'>
         <div className="">
-          <h6 className='text-white font-normal mb-[20px]'>Lorem ipsum dolor.</h6>
-          <h2 className='h1 text-white mb-[30px]'>Let&#39;s talk.</h2>
+          <h6 className='text-white font-normal mb-[20px]'>Lorem ipsum dolor</h6>
+          <h2 className='h1 text-white mb-[30px]'>Let&#39;s talk <span className="circle">.</span></h2>
           <div>
             <Link className="pink-btn" href="#">Contact us</Link>
           </div>
