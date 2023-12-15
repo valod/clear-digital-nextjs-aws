70f336d7b7bf777c2268250ed6579f986a2e132b
Abhay <abhay@cleardigital.com>
2023-12-15 20:08:12 +0530

    remove halfslider

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -2,9 +2,7 @@ import { NextSeo } from 'next-seo';
 import Layout from '../../components/layout';
 import BannerSecond from '../../components/bannerSecond';
 import Footercta from '../../components/Footercta';
-// import Coltwoslider from '../../components/Coltwoslider'; 
 import Slider from '../../components/logowithslider';
-// import Halfslider from '../../components/halfslider';
 import Logowithcontent from '../../components/logowithcontent';
 import Cta from '../../components/ctaoverlay';
 export default function Work({ menuItems }) {
@@ -17,9 +15,7 @@ export default function Work({ menuItems }) {
             <BannerSecond content='work' />
             <Logowithcontent />
             <Cta />
-            {/* <Coltwoslider/> */}
             <Slider />
-            {/* <Halfslider/> */}
             <Footercta overlay={'semi'}/>
         </Layout>
     )
