37aeb4eba3989194ab09897c5d63a3c1738f6f52
Ganesh <ganesh@webenertia.com>
2023-12-16 20:10:32 +0530

    matched overview

--- components/ContentGridserviceDetail.jsx
+++ components/ContentGridserviceDetail.jsx
@@ -9,41 +9,41 @@ const ContentGrid = () => {
                 <div className='intro'>
                     <div className='wrapper'>
                         <div className='intro__text'>
-                            <h6 className='mb-[18px]'>
+                            <h6 className='subtitle mb-[16px]'>
                                 Overview
                             </h6>
                             <h2>
                                 Great digital strategy <span className="underline-black text-pink">answers</span> critical questions.
                             </h2>
-                            <p className='mt-48px'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+                            <p className='mt-[48px] max-w-[81%]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
                         </div>
                     </div>
                 </div>
-                <div className='mainRow flex flex-wrap mt-[52px] -ml-[10px]'>
+                <div className='mainRow flex flex-wrap mt-[36px] -ml-[10px]'>
                     <div className='w-threeCard relative pr-[90px] mx-2.5 md:w-full xl:pr-[50px] tablet:w-halfWidth tablet:mb-[50px] md:mb-[50px]'>
                         <Link href={'#'} className='redirect'>  .  </Link>
-                        <div className='max-w-[79px] max-h[79px] mb-[34px] w-full'>
+                        <div className='max-w-[79px] max-h[85px] mb-[30px] w-full'>
                             <Image src={"/overview-1.png"} alt='' width={80} height={80} />
                         </div>
-                        <h4>Lorem ipsum amet dolor sit.</h4>
+                        <h4 className='mb-[10px]'>Lorem ipsum amet dolor sit.</h4>
                         <p className='medium'>Torem ipsum dolor sit amet, consectetur adipiscing at elit. Nunc vulputate libero dolore aliqua et.</p>
 
                     </div>
                     <div className='w-threeCard relative pr-[90px] mx-2.5 md:w-full xl:pr-[50px] tablet:w-halfWidth tablet:mb-[50px] md:mb-[50px]'>
                         <Link href={'#'} className='redirect'>  .  </Link>
-                        <div className='max-w-[79px] max-h[79px] mb-[34px] w-full'>
+                        <div className='max-w-[79px] max-h[85px] mb-[30px] w-full'>
                             <Image src={"/overview-2.png"} alt='' width={80} height={80} />
                         </div>
-                        <h4>Lorem ipsum amet dolor sit.</h4>
+                        <h4 className='mb-[10px]'>Lorem ipsum amet dolor sit.</h4>
                         <p className='medium'>Torem ipsum dolor sit amet, consectetur adipiscing at elit. Nunc vulputate libero dolore aliqua et.</p>
 
                     </div>
                     <div className='w-threeCard relative pr-[90px] mx-2.5 md:w-full xl:pr-[50px] tablet:w-halfWidth'>
                         <Link href={'#'} className='redirect'>  .  </Link>
-                        <div className='max-w-[79px] max-h[79px] mb-[34px] w-full'>
-                            <Image src={"/overview-3.png"} alt='' width={80} height={80} />
+                        <div className='max-w-[79px] max-h[85px] mb-[30px] w-full'>
+                            <Image src={"/overview-3.svg"} alt='' width={80} height={80} />
                         </div>
-                        <h4>Lorem ipsum amet dolor sit.</h4>
+                        <h4 className='mb-[10px]'>Lorem ipsum amet dolor sit.</h4>
                         <p className='medium'>Torem ipsum dolor sit amet, consectetur adipiscing at elit. Nunc vulputate libero dolore aliqua et.</p>
 
                     </div>
