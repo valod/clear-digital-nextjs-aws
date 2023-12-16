0aee3f6fa2f90411ea6276d5477815c49afeb952
Ganesh <ganesh@webenertia.com>
2023-12-16 11:10:08 +0530

    uncommented service overview

--- pages/service-overview/index.jsx
+++ pages/service-overview/index.jsx
@@ -1,12 +1,11 @@
 import { NextSeo } from 'next-seo';
 import Layout from '../../components/layout';
 import BannerSecond from '../../components/bannerSecond';
-// import Sticky from '../../components/sticky'
+import Sticky from '../../components/sticky'
 import IntroWithCards from "../../components/introWithCards"
 import Cta from '../../components/ctaoverlayServiceOverview';
 import Footercta from '../../components/Footercta';
 import Accordian from '../../components/accordianServiceOverview'
-// import ColTwoSlider from '@/components/colTwoSlider';
 export default function serviceOverview({ menuItems }) {
 	return (
 		<Layout footerMenu={menuItems}>
@@ -15,12 +14,11 @@ export default function serviceOverview({ menuItems }) {
 				description="ClearDigital"
 			/>
 			<BannerSecond content='serviceOverview' />
+			<Sticky />
 			<IntroWithCards />
 			<Cta />
-			{/* <Sticky /> */}
-			{/* <ColTwoSlider/> */}
-			{/* <Accordian />
-			<Footercta /> */}
+			<Accordian />
+			<Footercta />
 		</Layout>
 	)
 }
