a1c946d5da57294edab6f73d8d9483a476ed3476
Ganesh <ganesh@webenertia.com>
2023-12-16 15:19:23 +0530

    fixing build

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -1,7 +1,7 @@
 import { NextSeo } from 'next-seo';
 import Layout from '../../components/layout';
 import BannerSecond from '../../components/bannerSecond';
-import Slider from '../../components/logowithslider';
+// import Slider from '../../components/logowithslider';
 // import Introduction from '../../components/introduction';
 // import Logowithcontent from '../../components/logowithcontent';
 // import Cta from '../../components/ctaoverlay';
@@ -18,7 +18,7 @@ export default function Work({ menuItems }) {
             {/* <Introduction /> */}
             {/* <Logowithcontent contentName='data' /> */}
             {/* <Cta contentName={'work'} /> */}
-            <Slider />
+            {/* <Slider /> */}
 
         </Layout>
     )
