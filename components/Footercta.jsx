439d455ea7d381f9b7c1f05e3f58ac3cb21a92f9
Abhay <abhay@cleardigital.com>
2023-12-19 17:44:16 +0530

    update footercta

--- components/Footercta.jsx
+++ components/Footercta.jsx
@@ -5,12 +5,12 @@ import Link from "next/link";
 const FooterCta = ({ props }) => {
   return (
     <section className='footerCta grid__parallax height__full-nav '>
-      <div className='bg-img dark__overlay'>
+      <div className='bg-img light__overlay'>
         <Image src="/Image.svg" alt='image' height={100} width={100} className='w-full h-full object-cover' />
       </div>
       <div className='container'>
         <div className="">
-        <h6 className='text-white  mb-[20px] text-[25px] font-normal'>Lorem ipsum dolor</h6>
+        <h6 className='text-white  mb-[20px] subtitle font-normal'>Lorem ipsum dolor</h6>
           <h2 className='h1 text-white mb-[30px] font-weight-[700]'>Let&#39;s talk <span className="circle">.</span></h2>
           <div>
             <Link className="pink-btn" href="#">Contact us</Link>
