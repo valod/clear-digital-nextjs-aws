2789674803955f0b1bf6c437b38808c659df60a8
Abhay <abhay@cleardigital.com>
2023-12-19 15:20:37 +0530

    update intro

--- components/introduction.jsx
+++ components/introduction.jsx
@@ -6,14 +6,14 @@ const Introduction = () => {
 <section className="intro  padding-top">
       <div className="container">
         <div className="wrapper  w-full relative flex flex-wrap justify-between">
-          <div className="contentwrapper max-w-[43rem] pr-[10px]">
-            <h6 className="mb-[20px] font-normal">Case studies</h6>
+          <div className="contentwrapper max-w-[69rem] pr-[10px]">
+            <h6 className="mb-[20px] font-normal subtitle">Case studies</h6>
             <h3 className="font-bold">
               Nisl <span className="pink">pulvinar</span> consectet nec lipsum
               dolor
             </h3>
           </div>
-          <div className="btnwrapper mt-[50px] sm:mt-[30px]">
+          <div className="btnwrapper mt-[60px] sm:mt-[30px]">
             {/* Provide a valid URL or route in href */}
             <Link href="/case-studies" className="pink-btn">
               View all case studies
